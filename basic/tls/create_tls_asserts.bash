a276a60b898bfc6d5eb2f356ab71d2a81614199f
Pengfei Ni <feiskyer@gmail.com>
2017-05-23 18:14:06 +0800

    update files

--- basic/tls/create_tls_asserts.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-openssl genrsa -out server.key 2048
-openssl req -nodes -new -key server.key -subj "/CN=localhost" -out server.csr
-openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
--- basic/tls/index.md
+++
@@ -1,7 +0,0 @@
-# TLS/SSL指南
-
-1. [TLS和安全通信](./tls.html)
-2. [OpenSSL操作指南](./openssl.html)
-3. [用Go语言写 HTTPS 程序](./golang.html)
-
-参考<https://github.com/k8sp/tls>。
